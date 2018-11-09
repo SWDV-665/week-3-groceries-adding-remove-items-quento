741807cf816f10a30901ccc0977066fa0365990b
Quentin O <qochieng@gmail.com>
2018-11-09 00:15:46 -0600

    Added AlertController
    Added alert controller to hold add item form. Created showAddItemPrompt() method that shows the form.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -29,14 +30,13 @@ export class HomePage {
     }
   ];
 
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
 
   }
 
   removeItem(item){
     console.log("Removing item .." + item.name);
-
-
+    
     // Show item removal message (toast)
     const toast = this.toastCtrl.create({
       message: 'Removing item ' + item.name,
@@ -44,6 +44,45 @@ export class HomePage {
     });
     toast.present();
     
+  }
+
+  addItem(){
+    console.log("Item added"); 
+    this.showAddItemPrompt();   
+  }
+
+  showAddItemPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      message: "Please enter an item",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked', data);
+            this.items.push(data);
+
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }
